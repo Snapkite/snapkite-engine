c449a75c0ffff3c699e6732b83a9da1c1345c2d0
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2015-01-30 18:34:21 +0000

    Fix indentation

--- controllers/tweet.js
+++ controllers/tweet.js
@@ -2,24 +2,24 @@ var Tweet = require('../models/Tweet');
 
 var save = function (tweet) {
 
-    var tweet = new Tweet({
-        id_str: tweet.id_str,
-        text: tweet.text,
-        user: {
-            id_str: tweet.user.id_str,
-            name: tweet.user.name,
-            screen_name: tweet.user.screen_name
-        },
-        entities: {
-            media_url: tweet.entities.media[0].media_url
-        }
-    });
+  var tweet = new Tweet({
+    id_str: tweet.id_str,
+    text: tweet.text,
+    user: {
+      id_str: tweet.user.id_str,
+      name: tweet.user.name,
+      screen_name: tweet.user.screen_name
+    },
+    entities: {
+      media_url: tweet.entities.media[0].media_url
+    }
+  });
 
-    tweet.save(function(error, tweet) {
-        if (error) {
-            console.error('[Snapkite] Failed to save tweet. Error: ' + error);
-        }
-    });
+  tweet.save(function(error, tweet) {
+    if (error) {
+      console.error('[Snapkite] Failed to save tweet. Error: ' + error);
+    }
+  });
 };
 
 module.exports = {
